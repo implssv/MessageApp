b305da5ce5528b07868743a4804f418b7eddbd27
implossive <implossive@gmail.com>
2017-02-27 22:38:12 +0100

    gem faker test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Message.create([{
+                      title: Faker::Lorem.sentence(3, false, 4),
+                      description: Faker::Lorem.paragraphs,
+                      user_id: 1
+
+                  }])
+end
