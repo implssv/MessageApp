7f48324b8f634a65d721afd3aa56652a16e6c217
implossive <implossive@gmail.com>
2017-02-28 13:31:53 +0100

    route 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from(ActionController::RoutingError) { render :text => 'This is a custom 404.' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :messages
   root 'messages#index'
+  get "*path", to: redirect('/')
 end
