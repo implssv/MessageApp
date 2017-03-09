4b4598c75322e9758218f3915166861cbe1c133e
implossive <implossive@gmail.com>
2017-03-09 15:41:26 +0100

    Turbolink removing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require_tree .
 //= require bootstrap-sprockets
 //= require md_simple_editor
-//= require turbolinks
+
 
