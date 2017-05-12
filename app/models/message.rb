ea7d93c5876747eb501161e5ff5d9590449237c0
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 23:14:11 +0200

    no message

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,23 @@ class Message < ApplicationRecord
   belongs_to :category
   mount_uploader :cover, CoverUploader
 
-  validates_integrity_of  :cover
-  validates_processing_of :cover
+  attr_accessor :cover
 
-  private
-  def cover_size_validation
-    errors[:cover] << "should be less than 500KB" if cover.size > 0.5.megabytes
+  after_save :save_cover_image, if:cover
+
+
+  def save_cover_image
+    filename = cover.original_filename
+    folder = "public/messages/#{id}/cover"
+
+    FileUtils::mkdir_p folder
+
+    f = File.open File.join(folder, filename), "wb"
+    f.write cover.read()
+    f.close
+
+    self.cover = nil
+    update cover_filename: filename
   end
 
 end
