304fe131cad53c6c3f386a8c90786ac19c52478d
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 16:46:11 +0200

    no message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :find_message, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
 
   def index
     # @search = Message.search do
