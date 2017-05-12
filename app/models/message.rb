54b92fb3226e0486ef07ad1946a663e2d66e28af
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 23:20:29 +0200

    no message

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,21 +6,21 @@ class Message < ApplicationRecord
 
   #attr_accessor :cover
 
-  after_save :save_cover_image, if:cover
-
-
-  def save_cover_image
-    filename = cover.original_filename
-    folder = "public/messages/#{id}/cover"
-
-    FileUtils::mkdir_p folder
-
-    f = File.open File.join(folder, filename), "wb"
-    f.write cover.read()
-    f.close
-
-    self.cover = nil
-    update cover_filename: filename
-  end
+  # after_save :save_cover_image, if:cover
+  #
+  #
+  # def save_cover_image
+  #   filename = cover.original_filename
+  #   folder = "public/messages/#{id}/cover"
+  #
+  #   FileUtils::mkdir_p folder
+  # 
+  #   f = File.open File.join(folder, filename), "wb"
+  #   f.write cover.read()
+  #   f.close
+  #
+  #   self.cover = nil
+  #   update cover_filename: filename
+  # end
 
 end
