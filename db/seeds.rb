5c378897226fb2f472fcdcaabf430e0f2fb4811b
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 23:27:36 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ end
                       title: Faker::Commerce.product_name,
                       description: Faker::Hacker.say_something_smart,
                       category_id: Faker::Base::rand_in_range(1, 10),
-                      cover: Faker::Avatar.image,
+                      cover: open(Faker::Avatar.image),
                       user_id: 1
 
                   }])
