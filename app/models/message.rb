0d4805ed52f559e402afaf262a1c3d3cb710d12a
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 17:18:44 +0200

    no message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,12 @@
 class Message < ApplicationRecord
-  searchable do
-    text :title
-  end
-
   belongs_to :user
   has_many :comments
   belongs_to :category
   mount_uploader :cover, CoverUploader
+
+  searchable do
+    text :title
+  end
 
   validates_integrity_of  :cover
   validates_processing_of :cover
