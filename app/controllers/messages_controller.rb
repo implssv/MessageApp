08311195c354ac8e946b8a0feca6c9360721bb66
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 22:58:06 +0200

    no message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,43 +2,15 @@ class MessagesController < ApplicationController
   before_action :find_message, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show, :search]
 
-
-  #@messages = search(params[:search])
-
   def index
-
     if(params.has_key?(:search))
       @messages = Message.joins(:category).where('categories.name LIKE ?', params[:search]).paginate(:page => params[:page], :per_page => 9)
-
     else
       @messages = Message.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 9)
     end
-
-
   end
-
-
-
-  def self.search(search)
-    if search
-
-    else
-      find(:all)
-    end
-  end
-
-
-
-  # def index
-  #   @messages = Message.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 9)
-  # end
-  #
-  # def search
-  #   @messages = Message.where(["title LIKE ?", "%#{params[:search]}"])
-  # end
 
   def show
-
   end
 
   def new
@@ -59,9 +31,7 @@ class MessagesController < ApplicationController
 
   def edit
     if current_user.id != @message.user.id
-
       redirect_to root_path
-
     end
   end
 
@@ -78,13 +48,12 @@ class MessagesController < ApplicationController
       redirect_to message_path
     else
       render 'edit'
-
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:title, :description)
+    params.require(:message).permit(:title, :description, :cover)
   end
 
   def find_message
