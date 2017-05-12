7458d8dc10551fbff9065c08dd959af87bd6084a
Maksymilian Kicki <Maksymilian.kicki@gmail.com>
2017-05-12 19:28:33 +0200

    no message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
   def index
 
     if(params.has_key?(:search))
-      @messages = Message.search(params[:search])
+      @messages = search(params[:search])
     else
       @messages = Message.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 9)
     end
