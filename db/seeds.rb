3a5050db2693c580b6ab27cb3e86acb331a15b7d
Grzegorz Szymanowski <gwszymanowski@gmail.com>
2017-05-14 23:13:00 +0200

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,8 @@ end
                       title: Faker::Commerce.product_name,
                       description: Faker::Hacker.say_something_smart,
                       category_id: Faker::Base::rand_in_range(1, 10),
-                      cover: File.open(File.join(Rails.root, 'images/1.jpg' )),
+                      cover: File.open(Dir.glob(File.join(Rails.root, '1', '*')).sample),
+                      #cover: File.open(File.join(Rails.root, 'images/1.jpg' )),
                       user_id: 1
 
                   }])
